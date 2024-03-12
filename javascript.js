de5b838c738a0433a84f111e537434590dc7a01f
Soumil Biswas <soumil.biswas19@gmail.com>
2024-03-12 17:10:03 +0530

    Updated calculator logic, fixed display bugs.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 // Driver Code
 const buttons = document.querySelectorAll('button');
+var a = "";
+var b = "";
+var op = "";
 
 for (let button of buttons){
     button.addEventListener("click", function(e) {      //calculator logic
         let i = e.target.textContent;   
-        console.log(i);
-        let a,b,op = "";
-        if (i == "+", "-", "/", "*"){   //dealing with operand input
+        //console.log(i, typeof i);
+        if (i == "+" || i == "-" || i == "/" || i == "*"){    //dealing with operand input
             if (a == ""){
                 if(i == "-"){
                     a = "0";
@@ -16,36 +18,69 @@ for (let button of buttons){
             else if (b != ""){          //dealing with negative numbers
                 a = operate(a, b, op);
                 b = "";
+                //console.log("displaying a from negative check")
                 display(a);
             }
             op = i;
+            display(a+" "+i);
         }
         else if (i == "="){             //dealing with "=" input
             if (b != ""){
                 a = operate(a, b, op);
                 b = "";
-                display(a);
+                //console.log("displaying a from result check")
+                display(a);   
             }
         }
-        else if (a == ""){              //dealing with numerial input
+        else if (i == "Clear"){         //clear or backspace
+            if (op == "") {
+                a = a.slice(0, -1);
+                console.log("a = ", a);
+                display(a);
+            }
+            else {
+                b = b.slice(0,-1);
+                console.log("b = ", b);
+                display(a+" "+op+" "+b);                
+            }
+        }
+        else if (i == "AC"){            //AC: All Clear
+            a = "";
+            b = "";
+            op = "";
+            display("");
+        }
+        else if (op == ""){              //dealing with numerial input
             a = a + i;
+            console.log("a = ", a);
             display(a);
         }
-        else b = b + i;
-        display(b);
+        else {
+            b = b + i;
+            console.log("b = ", b);
+            display(a+" "+op+" "+b);
+        }
     });
 }
 
-function operate(a, b, op){
+function operate(a, b, si){     //function for arithmetic operation
     a = Number(a);
     b = Number(b);
-    if(op == "/") return a/b;
-    else if (op == "*") return a*b;
-    else if (op == "+") return a+b;
+    if(si == "/") {
+        if(b == 0){
+            alert("no.");
+            op = "";
+            return "";
+        }
+        else return a/b;
+    }
+    else if (si == "*") return a*b;
+    else if (si == "+") return a+b;
     else return a-b;
 }
 
-function display(number){
+function display(number){       //function to display output on screen
+    //console.log("sum is = ", number);
     const screen = document.querySelector(".screen");
-    screen.setAttribute("textContent", number);
+    screen.textContent = number;
 }
